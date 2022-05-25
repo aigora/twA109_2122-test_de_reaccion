421143f93e3f705baf27799effee08d9d1d9248f
PabloSanchz <80348065+PabloSanchz@users.noreply.github.com>
2022-05-25 21:25:59 +0200

    Update Codigo_Visual.cpp

--- Codigo_Visual.cpp
+++ Codigo_Visual.cpp
@@ -13,20 +13,20 @@ typedef struct {
 	float time;
 }sujeto;
 
-int menu_ppal(void);
 void alta_sujetos(sujeto* a, int num);
 void crear_fichero(sujeto* a, int num);
 float verifica_sensores(Serial* Arduino, char* port);
-float leer_tiempo(Serial* Arduino);
 int Enviar_y_Recibir(Serial* Arduino, const char* mensaje_enviar, char* mensaje_recibir);
 float float_from_cadena(char cadena[]);
+float lanzar_test(Serial* Arduino);
+int Recibir(Serial* Arduino, char* mensaje_recibir);
 
 
 int main(void) {
 	Serial* Arduino;
 	sujeto* v;
-	char puerto[] = "COM6";
-	int i = 0, n, edad, opc, hay_valores = 0, posible = 0;
+	char puerto[] = "COM3";
+	int i = 0, n, edad, opcion, hay_valores = 0, posible = 0;
 	char nombre[N], apellido[N], csalida, intro;
 	Arduino = new Serial((char*)puerto);
 	printf("¿Cuantos sujetos van a realizar la prueba?\n");
@@ -37,16 +37,24 @@ int main(void) {
 	else {
 		do
 		{
-			opc = menu_ppal();
-			if (hay_valores == 0 && opc > 1 && opc < 4)
-				printf("\nEs necesario -> 1- Tomar datos personales.\n");
+			printf("\nTEST DE REACCION");
+			printf("\n========================");
+			printf("\n1 - Tomar datos personales.");
+			printf("\n2 - Iniciar test");
+			printf("\n3 - Crear registro");
+			printf("\n4 - Salir");
+			printf("\nIntroduzca opcion:");
+			scanf_s("%d", &opcion);
+
+			if (hay_valores == 0 && opcion > 1 && opcion < 6)
+				printf("\nAntes de hacer operaciones es necesario -> 1- Tomar datos personales\n");
 			else
-				switch (opc) {
+				switch (opcion)
+				{
 				case 1:
 					hay_valores = 1;
 					alta_sujetos(v, n);
 					break;
-					opc = menu_ppal();
 				case 2:
 					if (i < n) {
 						v[i].time = verifica_sensores(Arduino, puerto);
@@ -58,7 +66,6 @@ int main(void) {
 					else {
 						printf("\nNo hay memoria para mas test\n");
 					}
-					opc = menu_ppal();
 					break;
 				case 3:
 					if (posible == 0) {
@@ -67,45 +74,15 @@ int main(void) {
 					else {
 						crear_fichero(v, n);
 					}
-					opc = menu_ppal();
 					break;
 				case 4:
-					if (posible == 0) {
-						printf("\n¿Seguro que quieres salir del programa sin realizar ninguna prueba?\n");
-						printf("Los datos introducidos previamente se perderán\n");
-						printf("S/N\n");
-						scanf_s("%c", &csalida);
-						scanf_s("%c", &intro);
-						if (csalida == 'S') {
-							opc = 5;
-							break;
-						}
-					}
-					else {
-						break;
-					}
+					break;
 				default:
-					printf("Opcion inexistente\n");
+					printf("\nOpcion inexistente\n");
 				}
-		} while (opc != 5);
+		} while (opcion != 4);
 	}
 	return 0;
-}
-
-int menu_ppal(void)
-{
-	int opcion;
-	char intro;
-	printf("\n");
-	printf("Menú Principal\n");
-	printf("==============\n");
-	printf("1-Tomar datos personales.\n");
-	printf("2-Iniciar test\n");
-	printf("3-Crear registro\n");
-	printf("4-Salir\n");
-	scanf_s("%d", &opcion);
-	scanf_s("%c", &intro);
-	return opcion;
 }
 
 void alta_sujetos(sujeto* a, int num)
@@ -149,7 +126,8 @@ float verifica_sensores(Serial* Arduino, char* port)
 	float tiempo = 0;
 	if (Arduino->IsConnected())
 	{
-		tiempo = leer_tiempo(Arduino);
+		start(Arduino);
+		tiempo = lanzar_test(Arduino);
 		if (tiempo != -1)
 			printf("\nTiempo: %f\n", tiempo);
 	}
@@ -157,26 +135,6 @@ float verifica_sensores(Serial* Arduino, char* port)
 	{
 		printf("\nNo se ha podido conectar con Arduino.\n");
 		printf("Revise la conexión, el puerto %s y desactive el monitor serie del IDE de Arduino.\n",port);
-	}
-	return tiempo;
-}
-
-float leer_tiempo(Serial* Arduino)
-{
-	float tiempo;
-	int bytesRecibidos;
-	char mensaje_recibido[MAX_BUFFER];
-
-	bytesRecibidos = Enviar_y_Recibir(Arduino, "GET_TIEMPO\n", mensaje_recibido);
-	if (bytesRecibidos <= 0)
-	{
-		printf("\nNo se ha recibido respuesta a la petición\n");
-		tiempo = -1;
-	}
-	else
-	{
-		printf("\nLa respuesta recibida tiene %d bytes. Recibido=%s\n", bytesRecibidos, mensaje_recibido);
-		tiempo = float_from_cadena(mensaje_recibido);
 	}
 	return tiempo;
 }
@@ -239,4 +197,63 @@ float float_from_cadena(char cadena[]) {
 		}
 	}
 	return numero;
+}
+
+float lanzar_test(Serial* Arduino)
+{
+
+	float tiempo;
+	int bytesRecibidos;
+	char mensaje_recibido[MAX_BUFFER];
+
+	if (Arduino->IsConnected())
+	{
+		bytesRecibidos = Enviar_y_Recibir(Arduino, "START_TEST\n", mensaje_recibido);
+		if (bytesRecibidos <= 0)
+		{
+			bytesRecibidos = Enviar_y_Recibir(Arduino, "GET_TIEMPO\n", mensaje_recibido);
+			if (bytesRecibidos <= 0)
+			{
+				printf("\nNo se ha recibido respuesta a la petición\n");
+				tiempo = -1;
+			}
+			else
+			{
+				printf("\nLa respuesta recibida tiene %d bytes. Recibido=%s\n", bytesRecibidos, mensaje_recibido);
+				bytesRecibidos = Recibir(Arduino, mensaje_recibido);
+				if (bytesRecibidos > 0)
+					tiempo = float_from_cadena(mensaje_recibido);
+				else
+				{
+					printf("Timeout sin recibir respuesta del test\n");
+					tiempo = -1;
+				}
+			}
+			return tiempo;
+		}
+	}
+}
+
+int Recibir(Serial* Arduino, char* mensaje_recibir)
+{
+	int bytes_recibidos = 0, total = 0;
+	int intentos = 0, fin_linea = 0;
+	bytes_recibidos = Arduino->ReadData(mensaje_recibir, sizeof(char) * MAX_BUFFER - 1);
+
+	while ((bytes_recibidos > 0 || intentos < 10) && fin_linea == 0)
+	{
+		if (bytes_recibidos > 0)
+		{
+			total += bytes_recibidos;
+			if (mensaje_recibir[total - 1] == 13 || mensaje_recibir[total - 1] == 10)
+				fin_linea = 1;
+		}
+		else
+			intentos++;
+		Sleep(2 * PAUSA_MS);
+		bytes_recibidos = Arduino->ReadData(mensaje_recibir + total, sizeof(char) * MAX_BUFFER - 1);
+	}
+	if (total > 0)
+		mensaje_recibir[total - 1] = '\0';
+	return total;
 }
